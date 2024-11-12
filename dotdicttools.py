79e6f19bbd947444d90482847d7babd7b667efce
dotdict <dotdict@elmer.ws>
2024-11-12 08:01:36 +0100

    Add support for lists and tuples

--- dotdicttools.py
+++ dotdicttools.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 
 class DotDict(dict):
@@ -11,4 +11,20 @@ class DotDict(dict):
             self[k] = v
 
     def __setitem__(self, k, v):
-        return dict.__setitem__(self, k, DotDict(v) if isinstance(v, dict) else v)
+        if isinstance(v, dict):
+            return dict.__setitem__(self, k, DotDict(v))
+        elif isinstance(v, (list, tuple)):
+            return dict.__setitem__(self, k, DotList(v))
+        else:
+            return dict.__setitem__(self, k, v)
+
+
+class DotList(list):
+    def __init__(self, l):
+        for x in l:
+            if isinstance(x, dict):
+                self.append(DotDict(x))
+            elif isinstance(x, (list, tuple)):
+                self.append(DotList(x))
+            else:
+                self.append(x)
